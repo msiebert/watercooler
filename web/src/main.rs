bd2ab0252a5d09c26b271dac067062ff3df01d38
Mark Siebert <mark@bluematador.com>
2020-06-27 21:19:30 -0600

    serve the ts from a rocket template

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,12 +1,20 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+use std::collections::HashMap;
+use rocket_contrib::templates::Template;
+
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context: HashMap<String, String> = HashMap::new();
+    Template::render("index", &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
