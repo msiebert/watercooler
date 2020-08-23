5628bde3180757301e91be0493fcd084191d3fc5
Mark Siebert <mark@bluematador.com>
2020-08-23 12:13:45 -0600

    connect to db from rust

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,20 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+
+use rocket_contrib::databases::diesel::PgConnection;
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 
-#[macro_use]
-extern crate rocket;
-
 #[get("/")]
-fn index() -> Template {
+fn index(_db_conn: DbConn) -> Template {
     let context: HashMap<String, String> = HashMap::new();
     Template::render("index", &context)
 }
 
+#[database("watercooler")]
+struct DbConn(PgConnection);
+
 fn main() {
     rocket::ignite()
         .mount("/", routes![index])
+        .attach(DbConn::fairing())
         .attach(Template::fairing())
         .launch();
 }
